786ef92c3e8fc3e129c2e56bd0837652daae63d3
Xiangyu Zhu <frefreak.zxy@gmail.com>
2016-05-05 13:09:59 +0800

    use `Text.Hamlet.Runtime` for easier development

--- server/src/Application.hs
+++ server/src/Application.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds, TypeOperators #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Application where
 
@@ -8,11 +9,21 @@ import Data.Text
 import Servant
 import Servant.HTML.Blaze
 import Text.Hamlet
+import Text.Hamlet.Runtime
 import Network.Wai
+import qualified Data.Map as M
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import Text.Blaze.Html (toHtml)
+import Control.Monad.Catch
+import Control.Monad.IO.Class
 
 import qualified JSON as J
+
+devTemplate :: (MonadThrow m, MonadIO m) => FilePath -> m HamletTemplate
+devTemplate = readHamletTemplateFile defaultHamletSettings
+
+runtimeHtml :: (MonadThrow m, MonadIO m) => FilePath -> M.Map Text HamletData -> m Html
+runtimeHtml fp m = devTemplate fp >>= flip renderHamletTemplate m
 
 type CliperApi = "register" :> Get '[HTML] Html
             {-:<|> "register" :> Post '[JSON] J.JRegister-}
@@ -21,9 +32,10 @@ cliperServer :: Server CliperApi
 cliperServer =  registerGetServer
         {-:<|>    registerPostServer-}
 
-registerGetServer = return . toHtml $ $(shamletFile "templates/register.hamlet")
+registerGetServer = runtimeHtml "templates/register.hamlet" $
+    M.singleton "username" "adv_zxy"
 
-registerPostServer = undefined
+{-registerPostServer = undefined-}
 
 cliperAPI :: Proxy CliperApi
 cliperAPI = Proxy
