828e68408a8c15315f2580ac105568dc98762287
Xiangyu Zhu <frefreak.zxy@gmail.com>
2016-05-05 14:04:57 +0800

    made some test to hamlet template

--- server/src/Application.hs
+++ server/src/Application.hs
@@ -5,6 +5,7 @@
 
 module Application where
 
+import Control.Monad.Trans.Either
 import Data.Text
 import Servant
 import Servant.HTML.Blaze
@@ -14,7 +15,7 @@ import Network.Wai
 import qualified Data.Map as M
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import Text.Blaze.Html (toHtml)
-import Control.Monad.Catch
+import Control.Monad.Catch (MonadThrow)
 import Control.Monad.IO.Class
 
 import qualified JSON as J
@@ -25,15 +26,21 @@ devTemplate = readHamletTemplateFile defaultHamletSettings
 runtimeHtml :: (MonadThrow m, MonadIO m) => FilePath -> M.Map Text HamletData -> m Html
 runtimeHtml fp m = devTemplate fp >>= flip renderHamletTemplate m
 
-type CliperApi = "register" :> Get '[HTML] Html
+type CliperApi =                    Get '[HTML] Html
+                :<|> "register" :> Get '[HTML] Html
             {-:<|> "register" :> Post '[JSON] J.JRegister-}
 
+type Handler = EitherT ServantErr IO
+
 cliperServer :: Server CliperApi
-cliperServer =  registerGetServer
+cliperServer = homeServer :<|> registerGetServer
         {-:<|>    registerPostServer-}
 
-registerGetServer = runtimeHtml "templates/register.hamlet" $
-    M.singleton "username" "adv_zxy"
+homeServer :: EitherT ServantErr IO Html
+homeServer = runtimeHtml "templates/home.hamlet" mempty
+
+registerGetServer :: Handler Html
+registerGetServer = runtimeHtml "templates/register.hamlet" mempty
 
 {-registerPostServer = undefined-}
 
